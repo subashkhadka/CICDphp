b96126358dae1465793849f270a5e9686129e401
subashkhadka <subash.khadka@epfnepal.com.np>
2020-10-15 16:20:20 +0545

    again commit

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
-echo "CICD pipeline testing of php code using apache webserver git jenkins."
+echo "CICD pipeline testing of php code using apache webserver git jenkins.";
 echo "CICD pipeline testing going on."
 ?>
